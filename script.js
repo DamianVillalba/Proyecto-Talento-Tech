1ef72afd30c11826e0d71e870b0f4c2732cad2db
DamianVillalba <damianalumno@gmail.com>
2024-12-20 16:23:02 -0300

    feat: ciclo de productos

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+//PRODUCTOS EN CONSOLA
+const productos = [];
+for(let i = 0; i < 5; i++){
+    const producto = {
+        id: i+1,
+        nombre: "Producto " + (i+1),
+        precio: Math.round(Math.random()*55000),
+        descripcion_corta: "Termo de acero inoxidable de 1 litro, ideal para mantener tus bebidas a la temperatura ideal por más tiempo. Diseño elegante y duradero. Perfecto para llevar a cualquier lugar",
+        descripcion_larga: "Nuestro termo de acero inoxidable de 1 litro es el compañero perfecto para tu día a día. Fabricado con materiales de alta calidad, este termo cuenta con una doble pared de acero inoxidable que proporciona un aislamiento superior, manteniendo tus bebidas calientes por hasta 24 horas y frías por hasta 24 horas. Su diseño elegante y moderno lo hace ideal para cualquier ocasión, ya sea en la oficina, en el gimnasio o durante una aventura al aire libre. Además, su tamaño compacto y ligero lo hacen fácil de transportar."
+    }
+    productos.push(producto);
+}
+
+productos.forEach((producto) => {
+    console.log(producto);
+})
+
